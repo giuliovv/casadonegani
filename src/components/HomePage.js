a96b69905fc381763e96717e55af0f5e55b9a057
giulio <giulio09@marcov.it>
2021-03-10 18:28:05 +0000

    Registro

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,9 +1,14 @@
 import * as React from "react"
 
+import { makeStyles } from '@material-ui/core/styles';
+
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import Snackbar from '@material-ui/core/Snackbar';
 import Typography from '@material-ui/core/Typography';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 
 import CloseIcon from '@material-ui/icons/Close';
 
@@ -27,15 +32,30 @@ if (typeof window!== "undefined" && !firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);
 }
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+      width: '50%',
+      maxWidth: 360,
+      backgroundColor: theme.palette.background.paper,
+      position: 'relative',
+      margin: "auto",
+      overflow: 'auto',
+      maxHeight: 300,
+    },
+  }));
+
 const HomePage = (props) => {
+    const classes = useStyles();
     const [value, onChange] = React.useState(new Date());
     const [date, setDate] = React.useState("");
+    const [timestamps, setTimestamps] = React.useState([""]);
     const [open, setOpen] = React.useState(false);
 
     const startDate = new Date(2021, 3, 10);
 
     const oneDay = 24 * 60 * 60 * 1000;
     const today = new Date();
+    today.setHours(0,0,0,0);
 
     function turnoDi(date){
         let diff = Math.round(Math.abs((startDate - date) / oneDay))
@@ -55,6 +75,15 @@ const HomePage = (props) => {
             })
     }
 
+    React.useEffect(
+        () => {
+            if(date != ""){
+                setTimestamps(date.map(data => data.data().data.toDate().getTime()))
+            }
+        },
+        [date],
+      );
+
     async function firma(){
         let utente = props.user
         const data = {
@@ -62,47 +91,69 @@ const HomePage = (props) => {
             user: utente,
           };
           
-          // Add a new document in collection "cities" with ID 'LA'
           await firebase.firestore().collection("bagno").doc(today.getTime().toString()).set(data).then(setOpen(true));
     }
   
     return(
         <div style={{display:"block"}}>
-            <Typography variant="h1" component="h2" gutterBottom>
-            {props.user !== "Laura" ? "Benvenuto" : "Benvenuta"} {props.user}
+            <div style={{minHeight: "100vh", display: "table"}}>
+                <div style={{display: "table-cell",  verticalAlign: "middle"}}>
+                    <Typography variant="h1" component="h2" gutterBottom style={{marginTop:"20px", marginBottom: "20px"}}>
+                    {props.user !== "Laura" ? "Benvenuto" : "Benvenuta"} {props.user}
+                    </Typography>
+                    <Calendar
+                        style={{magin:"auto"}}
+                        locale="it-IT"
+                        onChange={onChange}
+                        value={value}
+                        tileClassName={({ date, view }) => {
+                            if(timestamps.includes(date.getTime())){
+                                return "giornoFatto"
+                            }
+                            if(date.getTime() < today.getTime()){
+                                return
+                            }
+                            let diff = Math.round(Math.abs((startDate - date) / oneDay))
+                            if(Math.ceil(diff/4) % 3 === 0){
+                                return "highlightlaura"
+                            } else if(Math.ceil(diff/4) % 2 === 0){
+                                return "highlightfili"
+                            } else {
+                                return "highlightgiulio"
+                            }
+                        }}
+                        minDate={
+                            new Date()
+                        }
+                    />
+                    <Typography variant="overline" component="overline" display="block"  align="center" gutterBottom style={{width:"100vw", marginTop: "20px", marginBottom:"20px"}}>
+                    Giorno di {turnoDi(value)}
+                    </Typography>
+                    <Button variant="contained" color="primary" onClick={() => firma()}>
+                        Ho pulito
+                    </Button>
+                </div>
+            </div>
+
+            <hr/>
+
+            <Typography variant="h6" component="h6" display="block"  align="center" gutterBottom style={{width:"100vw", overflowX: "hidden"}}>
+            Registro
             </Typography>
-            <Calendar
-                style={{magin:"auto"}}
-                locale="it-IT"
-                onChange={onChange}
-                value={value}
-                tileClassName={({ date, view }) => {
-                    if(date.getTime() < today.getTime()-oneDay){
-                        return
-                    }
-                    let diff = Math.round(Math.abs((startDate - date) / oneDay))
-                    if(Math.ceil(diff/4) % 3 === 0){
-                        return "highlightlaura"
-                    } else if(Math.ceil(diff/4) % 2 === 0){
-                        return "highlightfili"
-                    } else {
-                        return "highlightgiulio"
-                    }
-                }}
-                minDate={
-                    new Date()
-                  }
-            />
-            <Typography variant="overline" component="overline" display="block"  align="center" gutterBottom style={{width:"100vw", marginTop: "20px", marginBottom:"20px"}}>
-            Giorno di {turnoDi(value)}
-            </Typography>
-            {
-                turnoDi(today) === props.user ?
-                <Button variant="contained" color="primary" onClick={() => firma()}>
-                    Fatto
-                </Button> :
-                null
-            }
+
+            <List dense={true} className={classes.root}>
+              {date == "" ? 
+              "Caricamento..." :
+              date.map( data =>
+                <ListItem>
+                  <ListItemText
+                    primary={Intl.DateTimeFormat('it-IT').format(data.data().data.toDate())}
+                    secondary={data.data().user}
+                  />
+                </ListItem>,
+              )}
+            </List>
+
             <Snackbar
                 anchorOrigin={{
                     vertical: 'bottom',
