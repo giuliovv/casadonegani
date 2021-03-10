8325f0ec4b98a1332770f8a729b11b53211f1a2a
giulio <giulio09@marcov.it>
2021-03-10 15:01:39 +0000

    Aggiunto calendario

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,21 +1,27 @@
 import * as React from "react"
-import { Link } from "gatsby"
+
 import Typography from '@material-ui/core/Typography';
 
-const HomePage = (props) => {
+import Calendar from 'react-calendar';
+import "../components/calendar.css"
 
-  return(
-      <div style={{display:"block"}}>
-        <Typography variant="h1" component="h2" gutterBottom>
-        {props.user != "Laura" ? "Benvenuto" : "Benvenuta"} {props.user}
-        </Typography>
-        <Typography variant="body1" gutterBottom>
-        body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
-        unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam
-        dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
-      </Typography>
-      </div>
-  )
+const HomePage = (props) => {
+    const [value, onChange] = React.useState(new Date());
+
+    return(
+        <div style={{display:"block"}}>
+            <Typography variant="h1" component="h2" gutterBottom>
+            {props.user !== "Laura" ? "Benvenuto" : "Benvenuta"} {props.user}
+            </Typography>
+            <Calendar
+                style={{magin:"auto"}}
+                locale="it-IT"
+                onChange={onChange}
+                showWeekNumbers
+                value={value}
+            />
+        </div>
+    )
 }
 
 export default HomePage
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,16 +5,41 @@ import SEO from "../components/seo"
 import LoginPage from "../components/LoginPage"
 import HomePage from "../components/HomePage"
 
+import red from '@material-ui/core/colors/red';
+import green from '@material-ui/core/colors/green';
+
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: red[500],
+    },
+    secondary: {
+      main: green[500],
+    },
+  },
+});
+
+
+theme.typography.h1 = {
+  fontSize: '13vw',
+  [`${theme.breakpoints.up('sm')} and (orientation: landscape)`]: {
+    fontSize: '8rem',
+  },
+};
+
 const IndexPage = () => {
   const [user, setUser] = React.useState(localStorage.getItem('username') || '');
 
   return(<Layout>
     <SEO title="Home" />
-    {user == "" ?
-      <LoginPage setUser={setUser}/> :
-      <HomePage user={user} />
-    }
-      
+    <ThemeProvider theme={theme}>
+      {user === "" ?
+        <LoginPage setUser={setUser}/> :
+        <HomePage user={user} />
+      }
+    </ThemeProvider>
     </Layout>
   )
 }
