c4eae2e8d9d8464c24ce6de35a63fe7745791a46
giulio <giulio09@marcov.it>
2021-03-10 16:14:13 +0000

    Giorni nel calendario

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,10 +3,28 @@ import * as React from "react"
 import Typography from '@material-ui/core/Typography';
 
 import Calendar from 'react-calendar';
+import moment from 'moment';
 import "../components/calendar.css"
 
 const HomePage = (props) => {
     const [value, onChange] = React.useState(new Date());
+
+    const startDate = new Date(2021, 3, 10);
+
+    const oneDay = 24 * 60 * 60 * 1000;
+    const today = new Date();
+
+    function turnoDi(date){
+        let diff = Math.round(Math.abs((startDate - date) / oneDay))
+        if(Math.ceil(diff/4) % 3 == 0){
+            return "Laura"
+        } else if(Math.ceil(diff/4) % 2 == 0){
+            return "Fili"
+        } else {
+            return "Giulio"
+        }
+    }
+  
 
     return(
         <div style={{display:"block"}}>
@@ -17,9 +35,27 @@ const HomePage = (props) => {
                 style={{magin:"auto"}}
                 locale="it-IT"
                 onChange={onChange}
-                showWeekNumbers
                 value={value}
+                tileClassName={({ date, view }) => {
+                    if(date.getTime() < today.getTime()-oneDay){
+                        return
+                    }
+                    let diff = Math.round(Math.abs((startDate - date) / oneDay))
+                    if(Math.ceil(diff/4) % 3 == 0){
+                        return "highlightlaura"
+                    } else if(Math.ceil(diff/4) % 2 == 0){
+                        return "highlightfili"
+                    } else {
+                        return "highlightgiulio"
+                    }
+                }}
+                minDate={
+                    new Date()
+                  }
             />
+            <Typography variant="body1" component="body1" gutterBottom>
+            Giorno di {turnoDi(value)}
+            </Typography>
         </div>
     )
 }
